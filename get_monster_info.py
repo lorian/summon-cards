397724c21c8a5555f2dc2c63517d625b96a3ece3
Lorian <lanthala@gmail.com>
2016-08-13 14:31:17 -0700

    First attempt at pulling out info; too many special cases

--- get_monster_info.py
+++ get_monster_info.py
@@ -4,28 +4,80 @@ import urllib
 import urllib2
 import pprint
 from bs4 import BeautifulSoup
+import unicodedata
 
-html = "<a> Keep me </a>"
+all_headers = ['Init',
+ 'Senses',
+ 'DEFENSE',
+ 'AC',
+ 'hp',
+ 'Fort',
+ 'Ref',
+ 'Will',
+ 'DR',
+ 'Immune',
+ 'Resist',
+ 'SR',
+ 'OFFENSE',
+ 'Speed',
+ 'Melee',
+ 'Ranged',
+ 'Special Attacks',
+ 'Spell-Like Abilities',
+ 'STATISTICS',
+ 'Str',
+ 'Dex',
+ 'Con',
+ 'Int',
+ 'Wis',
+ 'Cha',
+ 'Base Atk',
+ 'CMB',
+ 'CMD',
+ 'Feats',
+ 'Skills',
+ 'Languages',
+ 'SQ',
+ 'SPECIAL ABILITIES',]
+ignore_headers = ['DEFENSE', 'OFFENSE', 'STATISTICS']
 
-monster_urls = ['http://www.d20pfsrd.com/bestiary/monster-listings/animals/dinosaur/ankylosaurus', 'http://www.d20pfsrd.com/bestiary/monster-listings/outsiders/azata/bralani']
-
+monster_urls = ['http://www.d20pfsrd.com/bestiary/monster-listings/animals/dinosaur/ankylosaurus',
+				'http://www.d20pfsrd.com/bestiary/monster-listings/outsiders/azata/bralani']
+monster_info = dict()
 for url in monster_urls:
 	monster_page = urllib2.urlopen(url)
-	monster_soup = BeautifulSoup(monster_page)
+	monster_soup = BeautifulSoup(monster_page, "lxml")
+
 	print '\n\n'
-	print [text for text in monster_soup.find_all('table', class_="sites-layout-hbox")[-1].stripped_strings]
+	# monster stats are in last table of given class
+	info_table = [unicodedata.normalize('NFKD', text).encode('ascii','ignore')
+					for text in monster_soup.find_all('table', class_="sites-layout-hbox")[-1].stripped_strings]
+	pprint.pprint( info_table)
 
+	monster_info['name'] = info_table[0]
+	monster_info['size'] = info_table[3]
+	# take advantage of awkward but consistant list ordering
+	list_of_lookups = ['Init', 'Perception', 'Speed',
+						'hp', 'Fort', 'Ref', 'Will', 'DR', 'SR', 'Immune', 'Resist',
+						'Str', 'Dex', 'Con', 'Int', 'Wis', 'Cha', 'Base Atk', 'CMB', 'CMD',
+						'Languages', 'Reach']
+	for l in list_of_lookups:
+		try:
+			monster_info[l] = info_table[info_table.index(l) +1]
+		except:
+			print "missing {}".format(l)
+	# more complex lookups that could take up multiple table entries: (lookup, next entry)
+	list_of_lookups = [('AC','hp'), ('Senses','Perception'), ('Feats','Skills'),
+						('Melee','Ranged'), ('Melee','Space'), ('Melee','Special Attacks'),
+						('Ranged','Space'), ('Ranged','Special Attacks')]
+	for (s,e) in list_of_lookups:
+		try:
+			start_index = info_table.index(s) +1
+			end_index = info_table.index(e) -1
+			print info_table[start_index:end_index]
+		except:
+			print "missing {}/{}".format(s,e)
 
-'''
-bodytext = False
-monster_data = []
-for line in monster_page:
-	if line.startswith('<td align="left" width="85%">'):
-		bodytext = True
-	if 'sites-layout-tile' in line:
-		bodytext = False
-
-	if bodytext:
-		print BeautifulSoup(line).findAll(text=True)
-		#text = ''.join(text_parts)
-'''
+	# handle skills special since they have no clear end point: look for string, +num pairs
+	print '\n\n'
+	pprint.pprint(monster_info)
